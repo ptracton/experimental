a304061a8723fffce045511a4f559657e612e65b
ptracton <ptracton@ptracton-linuxmint14.(none)>
2012-12-04 23:28:58 -0800

    Latest changes

--- python3/config_files/src/config_file.py
+++ python3/config_files/src/config_file.py
@@ -5,15 +5,23 @@ Created on Dec 4, 2012
 '''
 
 import config_inherit
+import config_instance
 
 if __name__ == '__main__':
 
     inherit = config_inherit.config_inherit()
+    instance = config_instance.config_instance()
+
     inherit.read( "project_config.ini" )
+    instance.configparser.read( "project_config.ini" )
+
     sections_list = inherit.sections()
     for x in sections_list:
         print ( x )
         items_list = inherit.items( x )
         print( "%s %s" % ( x, items_list ) )
+
+
+    sections_list = instance.configparser.sections()
     pass
 

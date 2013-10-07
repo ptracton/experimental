83d12b780d862fd8c53b58d7111c7c4f4bc16840
ptracton <ptracton@gmail.com>
2013-10-07 14:26:18 -0700

    Split into a few files

---
+++ C/STM32/STM32F3/libopencm3_testing/includes/gpio.h
@@ -0,0 +1,8 @@
+
+#ifndef __gpio_h__
+#define __gpio_h__
+
+void gpio_setup(void);
+
+
+#endif
---
+++ C/STM32/STM32F3/libopencm3_testing/src/gpio.c
@@ -0,0 +1,20 @@
+
+#include <libopencm3/stm32/f3/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+void gpio_setup(void)
+{
+    /* Enable GPIOE clock. */
+    /* Manually: */
+    // RCC_AHB1ENR |= RCC_AHB1ENR_IOPDEN;
+    /* Using API functions: */
+    rcc_peripheral_enable_clock(&RCC_AHBENR, RCC_AHBENR_IOPEEN);
+
+    /* Set GPIO12 (in GPIO port E) to 'output push-pull'. */
+    /* Manually: */
+    //GPIOE_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2));
+    //GPIOE_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4));
+    /* Using API functions: */
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
+}

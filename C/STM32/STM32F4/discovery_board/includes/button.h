bd230eb29a1bcc9b7d3d43f3b2926dbcb906ae74
ptracton <ptracton@gmail.com>
2013-10-15 15:54:46 -0700

    Working on STM32F4

---
+++ C/STM32/STM32F4/discovery_board/includes/button.h
@@ -0,0 +1,14 @@
+
+#ifndef __BUTTON_H__
+#define __BUTTON_H__
+
+//
+// Must include #include "libopencm3/stm32/f4/gpio.h" before this file
+//
+
+#define BUTTON_USER  GPIO0
+
+void BUTTON_Init(void);
+
+
+#endif

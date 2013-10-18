62671ad20253680813770433eb432a9626d0c9c3
ptracton@gmail.com <ptracton@gmail.com>
2013-10-17 19:04:09 -0700

    Starting to bring in STM32F4 libraries from ST

---
+++ C/STM32/STM32F4/Libraries/STM32F4xx_StdPeriph_Driver/objects/stm32f4xx_cryp_aes.d
@@ -0,0 +1,22 @@
+objects/stm32f4xx_cryp_aes.o: src/stm32f4xx_cryp_aes.c \
+ inc/stm32f4xx_cryp.h ../CMSIS/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../CMSIS/Include/core_cm4.h \
+ /opt/CrossCompilers/gcc-arm-none-eabi-4_7-2013q1/bin/../lib/gcc/arm-none-eabi/4.7.3/include/stdint.h \
+ /opt/CrossCompilers/gcc-arm-none-eabi-4_7-2013q1/bin/../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/stdint.h \
+ /opt/CrossCompilers/gcc-arm-none-eabi-4_7-2013q1/bin/../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/_ansi.h \
+ /opt/CrossCompilers/gcc-arm-none-eabi-4_7-2013q1/bin/../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/newlib.h \
+ /opt/CrossCompilers/gcc-arm-none-eabi-4_7-2013q1/bin/../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/config.h \
+ /opt/CrossCompilers/gcc-arm-none-eabi-4_7-2013q1/bin/../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /opt/CrossCompilers/gcc-arm-none-eabi-4_7-2013q1/bin/../lib/gcc/arm-none-eabi/4.7.3/../../../../arm-none-eabi/include/sys/features.h \
+ ../CMSIS/Include/core_cmInstr.h ../CMSIS/Include/core_cmFunc.h \
+ ../CMSIS/Include/core_cm4_simd.h \
+ ../CMSIS/ST/STM32F4xx/Include/system_stm32f4xx.h inc/stm32f4xx_conf.h \
+ inc/stm32f4xx_adc.h inc/stm32f4xx_can.h inc/stm32f4xx_crc.h \
+ inc/stm32f4xx_cryp.h inc/stm32f4xx_dac.h inc/stm32f4xx_dbgmcu.h \
+ inc/stm32f4xx_dcmi.h inc/stm32f4xx_dma.h inc/stm32f4xx_exti.h \
+ inc/stm32f4xx_flash.h inc/stm32f4xx_fsmc.h inc/stm32f4xx_hash.h \
+ inc/stm32f4xx_gpio.h inc/stm32f4xx_i2c.h inc/stm32f4xx_iwdg.h \
+ inc/stm32f4xx_pwr.h inc/stm32f4xx_rcc.h inc/stm32f4xx_rng.h \
+ inc/stm32f4xx_rtc.h inc/stm32f4xx_sdio.h inc/stm32f4xx_spi.h \
+ inc/stm32f4xx_syscfg.h inc/stm32f4xx_tim.h inc/stm32f4xx_usart.h \
+ inc/stm32f4xx_wwdg.h inc/misc.h
--- C/STM32/STM32F4/libopencm3_testing/includes/gpio.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __gpio_h__
-#define __gpio_h__
-
-void gpio_setup(void);
-
-
-#endif

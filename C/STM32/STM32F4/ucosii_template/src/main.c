4e7669f7a97039c80397d169c384100f5b418754
ptracton <ptracton@gmail.com>
2013-11-01 12:04:27 -0700

    Forgot to add in ucos ii template

---
+++ C/STM32/STM32F4/ucosii_template/src/main.c
@@ -0,0 +1,14 @@
+#include <stm32f4xx.h>
+#include "includes.h"
+
+int main(void)
+{
+    
+    
+    //
+    // Start the OS, we do NOT return from here unless something has gone really wrong
+    //
+    OSStart(); 
+    
+    return 0;    
+}

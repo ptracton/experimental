d111faaf97eb7254936da99290983a8fb5cfeec6
ptracton <ptracton@gmail.com>
2014-10-09 22:59:59 -0700

    using new emacs environment with autopep8

--- python3/PyWeather/PyWeather.py
+++ python3/PyWeather/PyWeather.py
@@ -9,13 +9,14 @@ import json
 import urllib.request
 from pprint import *
 
+
 class PyWeather(object):
+
     '''
     classdocs
     '''
 
-
-    def __init__(self, key = "", location = ""):
+    def __init__(self, key="", location=""):
         '''
         Constructor
         '''
@@ -25,28 +26,29 @@ class PyWeather(object):
         self.DEBUG = False
         self.location = location
 
-        ##
-        ## Get the data
-        ##
+        #
+        # Get the data
+        #
 #        self._GetWeather()
 
     def _GetWeather(self):
         '''
         This is the function that acutally goes out to the website and fetches the data
         '''
-        
+
         self.timestamp = datetime.datetime.now()
-        weatherURL = 'http://api.worldweatheronline.com/free/v1/weather.ashx?key=%s&q=%s&num_of_days=3&format=json' % (self.key, self.location)
-    
+        weatherURL = 'http://api.worldweatheronline.com/free/v1/weather.ashx?key=%s&q=%s&num_of_days=3&format=json' % (
+            self.key, self.location)
+
         try:
             weather_page = urllib.request.urlopen(weatherURL)
-            
-            ##
-            ## http://stackoverflow.com/questions/6862770/python-3-let-json-object-accept-bytes-or-let-urlopen-output-strings
-            ##
+
+            #
+            # http://stackoverflow.com/questions/6862770/python-3-let-json-object-accept-bytes-or-let-urlopen-output-strings
+            #
             str_response = weather_page.readall().decode('utf-8')
             self.weather_json = json.loads(str_response)
-                    
+
         except:
             print("Failed to get page")
             return None
@@ -63,8 +65,5 @@ class PyWeather(object):
         self._GetWeather()
         return self.weather_json['data']['request']
 
-
     def _GetTimeZone(self):
         return
-
-

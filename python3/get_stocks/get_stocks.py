7794053360af425f1d805afe4910755fc9f5c546
ptracton <ptracton@gmail.com>
2014-11-04 23:42:29 -0800

    Returned get_stocks.py to project

---
+++ python3/get_stocks/get_stocks.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+"""
+This is a simple demo program to show the various ways to get stock data
+from Google or Yahoo
+"""
+
+import sys
+from PyQt4.QtGui import *
+import UI
+
+if __name__ == "__main__":
+    print("Getting Stocks")
+
+    app = QApplication(sys.argv)
+    GUI = UI.UI()
+    GUI.show()
+    app.exec_()

7ff33d7f06fd3994f987a34175f20756c0586167
ptracton <ptracton@gmail.com>
2014-11-20 14:16:09 -0800

    Moved Postgres to Library/Database

---
+++ python3/Library/Database/__init__.py
@@ -0,0 +1,15 @@
+
+
+"""
+This is the init for our Database Libary.
+"""
+
+__all__ = ['Postgres']
+
+import os
+import sys
+
+sys.path.append("/user/tractp1/scratch/src/experimental/python3/Library/Database")
+sys.path.append("/home/ptracton/src/software/experimental/python3/Library/Database")
+sys.path.append('c:\\Users\\tractp1\\src\\software\\experimental\\python3\\Library\\Database')
+import Postgres

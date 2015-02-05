82be6ea303f1e035f0a0d87ed726d0616f1a6433
Phil Tracton <ptracton@gmail.com>
2015-02-05 17:46:40 -0600

    * python3/nose/Test_GoogleFinance.py: nose test testing

--- python3/nose/Test_GoogleFinance.py
+++ python3/nose/Test_GoogleFinance.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import time
 import logging
-import nose
+import stat
+import nose.proxy
 import GoogleFinance
 
 
@@ -13,8 +15,10 @@ class Test_GoogleFinance:
         pass
 
     def setup(self):
-
-        self.google = GoogleFinance.GoogleFinance(symbol="goog", start_date="30 October 2014")
+        start_date = time.strptime("30 October 2014", "%d %B %Y")
+        self.google = GoogleFinance.GoogleFinance(
+            symbol="goog",
+            start_date=start_date)
         return
 
     def teardown(self):
@@ -26,17 +30,30 @@ class Test_GoogleFinance:
         #self.empty = GoogleFinance.GoogleFinance()
         #assert self.empty.symbol == None
         assert self.google.symbol == "GOOG"
-        assert self.google.start_date == "30 October 2014"
+        #assert self.google.start_date == "30 October 2014"
         return
 
     def test_stock(self):
         '''
         Test case
         '''
-        start_date = time.strptime("01 January 2015", "%d %B %Y")
-        g = GoogleFinance.GoogleFinance(symbol="appl", start_date=start_date)
-        assert g.symbol == "APPL"
-        g.get_historical_stock_data()
-        g.get_historical_stock_data(filename="test.csv")
-        del(g)
+        # self.google.get_historical_stock_data()
+        try:
+            os.remove("test.csv")
+        except FileNotFoundError:
+            pass
+
+        self.google.get_historical_stock_data(filename="test.csv")
+        size = 0
+        try:
+            csv = open("test.csv", "r")
+            lines = csv.readlines()
+            csv.close()
+            size = len(lines)
+        except:
+            size = 0
+        assert size > 1
+
+        os.chmod("test.csv", stat.S_IRUSR | stat.S_IROTH | stat.S_IRGRP)
+        self.google.get_historical_stock_data(filename="test.csv")
         return

04a751b790d933a3964664217ae6347d762f47db
Phil Tracton <ptracton@gmail.com>
2017-03-07 23:11:09 -0800

    Please enter the commit message for your changes. Lines starting

--- python3/LA_Data/city_data_panda.py
+++ python3/LA_Data/city_data_panda.py
@@ -6,15 +6,28 @@
 
 # https://dev.socrata.com/foundry/data.lacity.org/kh8g-6365
 
-import numpy as np
-import pandas as pd
-import datetime
-import urllib
- 
-from bokeh.plotting import *
-from bokeh.models import HoverTool
-from collections import OrderedDict
+# import numpy as np
+import json
+import os
+import pandas
 
-query = ("https://data.lacity.org/resource/kh8g-6365.json")
-raw_data = pd.read_json(query)	
-print (raw_data)
+
+if __name__ == "__main__":
+
+    json_file = "test.json"
+
+    if not os.path.isfile(json_file):
+        query = ("https://data.lacity.org/resource/kh8g-6365.json")
+        raw_data = pandas.read_json(query)
+        f = open('test.json', 'w')
+        json.dump(pandas.DataFrame.to_json(raw_data), f)
+    else:
+        with open(json_file, 'r') as infile:
+            top_level_dict = json.load(infile)
+        df = pandas.read_json(top_level_dict)
+        print(type(df))
+        print(df.info())
+        print(df.dtypes)
+        print(df.describe())
+        print(df.head())
+        print(df.tail(3))

ed70d0b16ff5c76180a72a9da1cfbae9830b5e45
Phil Tracton <ptracton@gmail.com>
2018-03-23 21:53:44 -0700

    putting JSON data into postgresql database via ORM

---
+++ python3/sqlalchemy/generate_data/ORM.py
@@ -0,0 +1,30 @@
+
+import sqlalchemy
+import sqlalchemy.ext.declarative
+import sqlalchemy.orm
+
+# Global Setup
+user = "postgres"
+password = "python"
+host = "localhost"
+port = 5432
+database = "postgres"
+url = 'postgresql://{}:{}@{}:{}/{}'
+db_string = url.format(user, password, host, port, database)
+db = sqlalchemy.create_engine(db_string)
+base = sqlalchemy.ext.declarative.declarative_base()
+
+class People(base):
+    __tablename__ = 'people'
+
+    PersonalNumber = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
+    FirstName = sqlalchemy.Column(sqlalchemy.String)
+    LastName = sqlalchemy.Column(sqlalchemy.String)
+    StreetAddress = sqlalchemy.Column(sqlalchemy.String)
+    City = sqlalchemy.Column(sqlalchemy.String)
+    Region = sqlalchemy.Column(sqlalchemy.String)
+    Zip = sqlalchemy.Column(sqlalchemy.String)
+    Country = sqlalchemy.Column(sqlalchemy.String)
+    Pin = sqlalchemy.Column(sqlalchemy.Integer)
+    Date = sqlalchemy.Column(sqlalchemy.String)
+    Email = sqlalchemy.Column(sqlalchemy.String)
---
+++ python3/sqlalchemy/generate_data/generate.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+
+import json
+import sqlalchemy
+import sqlalchemy.ext.declarative
+import sqlalchemy.orm
+import ORM
+
+if __name__ == "__main__":
+
+    json_file = open("dataMar-23-2018.json")
+    json_data = json.load(json_file)
+
+
+    Session = sqlalchemy.orm.sessionmaker(ORM.db)
+    session = Session()
+    ORM.base.metadata.create_all(ORM.db)
+
+    index = 0
+    for x in json_data['data']:
+        print("{} {} {} {} {} {} {} {} {} {} {} {}".format(
+            index, x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10]))
+        if index > 0:
+            person = ORM.People()
+            person.PersonalNumber = x[0]
+            person.FirstName = x[1]
+            person.LastName = x[2]
+            person.StreetAddress = x[3]
+            person.City = x[4]
+            person.Region = x[5]
+            person.Zip = x[5]
+            person.Country = x[6]
+            person.Pin = x[7]
+            person.Date = x[8]
+            person.Email = x[9]
+            try:
+                session.add(person)
+                session.commit()
+            except:
+                print("FAIL on {}".format(x))
+            del (person)
+        index = index + 1
+

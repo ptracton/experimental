b66fe94ac2cc494e399ea563b43b8bd10ce29247
Phil Tracton <ptracton@gmail.com>
2018-04-05 16:42:38 -0500

    displaying image in PyQt5

--- python3/movies/omdb/example.py
+++ python3/movies/omdb/example.py
@@ -1,9 +1,66 @@
 #! /usr/bin/env python3
 
 import os
-#https://pypi.python.org/pypi/omdb/0.9.1
+# https://pypi.python.org/pypi/omdb/0.9.1
 import omdb
+import sys
 import requests
+
+import PyQt5
+import PyQt5.QtCore
+import PyQt5.QtWidgets
+
+
+class UI_Widget(PyQt5.QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(UI_Widget, self).__init__(parent)
+        vbox = PyQt5.QtWidgets.QVBoxLayout()
+
+        label = PyQt5.QtWidgets.QLabel("MY LABEL")
+        pixmap = PyQt5.QtGui.QPixmap('avatar_poster.jpg')
+        myScaledPixmap = pixmap.scaled(
+            label.size(), PyQt5.QtCore.Qt.KeepAspectRatio)
+        # label.setPixmap(myScaledPixmap)
+        label.setPixmap(pixmap)
+        label.setScaledContents(False)
+        #label.resize(640, 480)
+        # print(pixmap.width())
+        # print(pixmap.height())
+
+        # label.move(100,100)
+        vbox.addWidget(label)
+        self.setLayout(vbox)
+        return
+
+
+class UI(PyQt5.QtWidgets.QMainWindow):
+
+    def __init__(self, parent=None):
+        super(UI, self).__init__(parent)
+
+        self.statusBar().showMessage('Menu Bar')
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        helpMenu = menubar.addMenu('&Help')
+
+        exitAction = PyQt5.QtWidgets.QAction('&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(PyQt5.QtWidgets.qApp.quit)
+        fileMenu.addAction(exitAction)
+
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAction)
+
+        self.central = UI_Widget()
+
+        self.setCentralWidget(self.central)
+        self.setWindowTitle('Statusbar')
+        self.show()
+
+        return
+
 
 if __name__ == "__main__":
     print("OMDB Example Code")
@@ -14,7 +71,10 @@ if __name__ == "__main__":
     print(avatar)
     avatar_post_url = avatar['poster']
     r = requests.get(avatar_post_url)
-    #https://www.codementor.io/aviaryan/downloading-files-from-urls-in-python-77q3bs0un
+    # https://www.codementor.io/aviaryan/downloading-files-from-urls-in-python-77q3bs0un
     open('avatar_poster.jpg', 'wb').write(r.content)
-    
-    
+
+    app = PyQt5.QtWidgets.QApplication(sys.argv)
+    gui = UI()
+    gui.show()
+    app.exec_()
